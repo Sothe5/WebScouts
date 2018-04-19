ce9364dd80e20e9102957048160f45106d97794e
Carles-Bordas <carles.bordas@hotmail.com>
2018-04-19 16:27:19 +0200

    Implement Main method

--- src/main/java/uma/sii/mcaddss/webscouts/WebScoutsMain.java
+++ src/main/java/uma/sii/mcaddss/webscouts/WebScoutsMain.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uma.sii.mcaddss.webscouts;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -15,7 +15,11 @@ public class WebScoutsMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("uma.sii.mcaddss.WebScouts.jar_PU");
+        EntityManager em = emf.createEntityManager();
+        
+        em.close();
+        emf.close();
     }
     
 }
