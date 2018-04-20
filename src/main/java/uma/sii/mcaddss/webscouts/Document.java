a91708e823507b02cff6e6507606d5d7ca2c3c69
kiwinut <sergioxw@gmail.com>
2018-04-20 14:32:05 +0200

    Add entity Document

---
+++ src/main/java/uma/sii/mcaddss/webscouts/Document.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uma.sii.mcaddss.webscouts;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author cloud
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="type")
+@Table(name="Documents")
+public class Document implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String type;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date upload_date;
+    private Long file_size; 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the file_size
+     */
+    public Long getFileSize() {
+        return file_size;
+    }
+
+    /**
+     * @param file_size the file_size to set
+     */
+    public void setFileSize(Long file_size) {
+        this.file_size = file_size;
+    }
+
+    /**
+     * @return the upload_date
+     */
+    public Date getUploadDate() {
+        return upload_date;
+    }
+
+    /**
+     * @param upload_date the upload_date to set
+     */
+    public void setUploadDate(Date upload_date) {
+        this.upload_date = upload_date;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Document)) {
+            return false;
+        }
+        Document other = (Document) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Documento con identificador " + id + " de tipo " + getType();
+    }
+    
+}
