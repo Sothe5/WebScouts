32246e009adaf3ab1b0d400be52b39c5a6ad889b
Carles-Bordas <carles.bordas@hotmail.com>
2018-05-11 20:41:34 +0200

    Add event.xhtml page and EventPage.java backing bean

--- src/main/java/uma/sii/mcaddss/webscouts/events/EventPage.java
+++ src/main/java/uma/sii/mcaddss/webscouts/events/EventPage.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.faces.context.FacesContext;
 import uma.sii.mcaddss.webscouts.entities.Comment;
 import uma.sii.mcaddss.webscouts.entities.Document;
 import uma.sii.mcaddss.webscouts.entities.Event;
+import uma.sii.mcaddss.webscouts.entities.EventAttendance;
+import uma.sii.mcaddss.webscouts.entities.Group_Scout;
 
 /**
  *
@@ -20,14 +20,22 @@ import uma.sii.mcaddss.webscouts.entities.Event;
 @RequestScoped
 public class EventPage implements Serializable {
 
-    private final String eventId;
+    private int eventId;
     private Event event;
 
     public EventPage() {
-        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        eventId = params.get("id");
-        
-        // TODO get Event from List of Events
+        event = new Event();
+        event.setName("Salida a los montes de Málaga");
+        event.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus ex massa, id commodo mi gravida non. Proin eu sapien vitae ligula malesuada ultricies. Nullam et suscipit sem, eu porttitor ex. Morbi porttitor faucibus leo vel tempor. Aliquam et euismod purus, sed porttitor enim. Praesent egestas est eu varius tempus. Praesent varius felis diam, sed hendrerit mauris ultricies sit amet. Fusce interdum elit a nisl aliquam fermentum.\n" +
+"\n" +
+"                    Quisque non enim eros. Integer fringilla, dolor a luctus feugiat, quam nisi pellentesque nibh, et congue arcu ligula sodales arcu. Praesent sem arcu, viverra tristique felis at, tempor pretium sem. Suspendisse potenti. Aenean ornare vehicula diam. Phasellus cursus scelerisque quam, in ultricies dui dictum vehicula. Sed gravida consectetur turpis, at ullamcorper urna pulvinar. ");
+        event.setGroupscout(new Group_Scout());
+        event.getGroupscout().setName("Manada");
+        event.setDate(new Date());
+    }
+    
+    public void setEventId(String eventId) {
+        this.eventId = Integer.parseInt(eventId);
     }
 
     public String getEventName() {
@@ -37,24 +45,48 @@ public class EventPage implements Serializable {
     public String getEventDescription() {
         return event.getDescription();
     }
+    
+    public Group_Scout getEventGroup() {
+        return event.getGroupscout();
+    }
 
     public List<Comment> getEventComments() {
         return event.getComments();
     }
 
-    public String getEventDateFormatted(String format) {
+    public String getEventDateFormatted() {
         SimpleDateFormat formatter;
-        try {
-            formatter = new SimpleDateFormat(format);
-        } catch (Exception e) {
-            formatter = new SimpleDateFormat("dd.MM.yyyy a las HH:mm");
-        }
+        formatter = new SimpleDateFormat("dd 'de' MMM ' de' yyyy, HH:mm");
         Date eventDate = event.getDate();
         return formatter.format(eventDate);
     }
 
     public List<Document> getNecessaryDocuments() {
         return event.getDocuments();
+    }
+    
+    public List<EventAttendance> getEventAttendees() {
+        return event.getAttendees();
+    }
+    
+    public int getAtendeesCount() {
+        return event.getAttendees().size();
+    }
+    
+    public void markAssistence() {
+        EventAttendance ea = new EventAttendance();
+        ea.setAttendanceStatus("YES");
+        ea.setEvent(event);
+        //ea.setUser(user); // Get current user
+        event.getAttendees().add(ea);
+    }
+    
+    public void markNoAssistence() {
+        EventAttendance ea = new EventAttendance();
+        ea.setAttendanceStatus("NO");
+        ea.setEvent(event);
+        //ea.setUser(user); // Get current user
+        event.getAttendees().add(ea);
     }
 
     public boolean isEnroled() {
