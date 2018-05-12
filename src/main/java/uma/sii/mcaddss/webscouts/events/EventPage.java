2de3bb3518deed4df0273554def880417459d909
kiwinut <sergioxw@gmail.com>
2018-05-12 16:27:27 +0200

    Implement comments in Event view

--- src/main/java/uma/sii/mcaddss/webscouts/events/EventPage.java
+++ src/main/java/uma/sii/mcaddss/webscouts/events/EventPage.java
@@ -2,6 +2,7 @@ package uma.sii.mcaddss.webscouts.events;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
@@ -11,6 +12,7 @@ import uma.sii.mcaddss.webscouts.entities.Document;
 import uma.sii.mcaddss.webscouts.entities.Event;
 import uma.sii.mcaddss.webscouts.entities.EventAttendance;
 import uma.sii.mcaddss.webscouts.entities.Group_Scout;
+import uma.sii.mcaddss.webscouts.entities.User_Scout;
 
 /**
  *
@@ -32,6 +34,27 @@ public class EventPage implements Serializable {
         event.setGroupscout(new Group_Scout());
         event.getGroupscout().setName("Manada");
         event.setDate(new Date());
+        
+        // Build users for comments:
+        User_Scout pablo = new User_Scout(Long.MIN_VALUE, "pablo", event.getGroupscout(), "pablo", "test@test.com", "test aa", new Date(2002,11,21));
+        User_Scout manuel = new User_Scout(Long.MIN_VALUE, "manuel", event.getGroupscout(), "manuel", "test2@test.com", "test aa", new Date(2004,10,21));
+        User_Scout elisa = new User_Scout(Long.MIN_VALUE, "elisa", event.getGroupscout(), "elisa", "test3@test.com", "test aa oo", new Date(2006,9,14));
+        
+        // Build comments
+        Comment comment1 = new Comment(pablo, event, "Hola a todos, yo no podré ir");
+        Comment comment2 = new Comment(manuel, event, "Vale");
+        Comment comment3 = new Comment(elisa, event, "Aaaa bieeen");
+        comment2.setTarget(comment1);
+        comment1.addReply(comment2);
+        List<Comment> commentList = new ArrayList<>();
+        commentList.add(comment1);
+        commentList.add(comment2);
+        commentList.add(comment3);
+        event.setComments(commentList);
+    }
+    
+    public Event getEvent() {
+        return event;
     }
     
     public void setEventId(String eventId) {
@@ -56,7 +79,7 @@ public class EventPage implements Serializable {
 
     public String getEventDateFormatted() {
         SimpleDateFormat formatter;
-        formatter = new SimpleDateFormat("dd 'de' MMM ' de' yyyy, HH:mm");
+        formatter = new SimpleDateFormat("dd-MM-yyyy, HH:mm");
         Date eventDate = event.getDate();
         return formatter.format(eventDate);
     }
@@ -94,4 +117,6 @@ public class EventPage implements Serializable {
         return false;
     }
 
+    /* Comments */ 
+    
 }
