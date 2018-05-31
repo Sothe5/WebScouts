0879b8d2f16fc54488b19ef96393afe49ff9cb17
kiwinut <sergioxw@gmail.com>
2018-05-31 09:47:23 +0200

    Add Solar Round bean

---
+++ WebScoutsEE-ejb/src/main/java/uma/sii/mcaddss/webscouts/bean/SolarRound.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uma.sii.mcaddss.webscouts.bean;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author blub
+ */
+@Stateless
+@LocalBean
+public class SolarRound {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
